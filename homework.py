269094760b15f92db0faaa27fceec1910fd992fb
Сергей <bazzz@list.ru>
2021-10-12 18:43:45 +0300

    1

--- homework.py
+++ homework.py
@@ -1,12 +1,22 @@
+import os
+import time
+import requests
 import logging
+import telegram
+from dotenv import load_dotenv
 
-...
+logging.basicConfig(
+    level=logging.INFO,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+    filemode='w'
+)
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
+load_dotenv()
 
-...
+PRACTICUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,36 +29,52 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    logging.info(f'message send {message}')
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def get_api_answer(url, current_timestamp):
-    ...
+    """Берем информацию от сервера."""
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
+    print('!!!!!!!!!!!!!!!!!', homework_statuses.text)
+    print('@@@@@@@@@@@@@@@', homework_statuses.status_code)
+    logging.info('server respond')
+    return homework_statuses.json()
+    
 
 
 def parse_status(homework):
-    verdict = ...
-    ...
-
+    verdict = HOMEWORK_STATUSES[homework.get('status')]
+    homework_name = homework.get('homework_name')
+    logging.info(f'got verdict {verdict}')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
-    homeworks = response.get('homeworks')
-    ...
+    hws = response.get('homeworks')
+    return hws
 
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    ...
+    current_timestamp = 0  #int(time.time())
+    url = ENDPOINT
     while True:
         try:
-            ...
+            gaa = get_api_answer(url, current_timestamp)
+            cr = check_response(gaa)
+            if cr:
+                for hw in cr:
+                    ps = parse_status(hw)
+                    send_message(bot, ps)
             time.sleep(RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error('Bot down')
+            bot.send_message(
+                chat_id=CHAT_ID, text=f'Сбой в работе программы: {error}'
+            )
             time.sleep(RETRY_TIME)
             continue
 
